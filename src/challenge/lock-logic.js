af8073f4e3066f6988e73c2a42fb1c8158ee928b
Kathy Morawski <kathymorawski@gmail.com>
2021-02-10 23:36:20 -0600

    Chore: Run ESlint and fix any outstanding errors

--- src/challenge/lock-logic.js
+++ src/challenge/lock-logic.js
@@ -7,20 +7,21 @@ const lockState = window.mobx.observable({
   wheels: [0, 0, 0, 0]
 })
 
-function changeDialValue(index, incrementBy) {
-
-  //Find value at the selected Index, then add incremeneted value to it
-  let selectedIndexValue = lockState.wheels[index];
-  let newValue = selectedIndexValue += incrementBy;
+function changeDialValue (index, incrementBy) {
+  // Find value at the selected Index, then add incremeneted value to it
+  let selectedIndexValue = lockState.wheels[index]
+  const newValue = selectedIndexValue += incrementBy
   // Remove old value at the selected index, and replace it with our new value
-  lockState.wheels.splice(index, 1, newValue);
+  lockState.wheels.splice(index, 1, newValue)
 
-  //Compare our current wheels array values to our secret combo, returning a new array with 'true' in that index if the values match
-  let compareToSecretCombo = lockState.wheels.map((value, index) => { if (value === SECRET_COMBO[index]) { return true } })
+  // Compare our current wheels array values to our secret combo, returning a new array with 'true' in that index if the values match
+  const compareToSecretCombo = lockState.wheels.map((value, index) => {
+    if (value === SECRET_COMBO[index]) { return true } else { return false }
+  })
 
-  //Once every value in our new array from our .map method, set locked state to false and redirect to our intern page
+  // Once every value in our new array from our .map method is true, set locked state to false and redirect to our intern page
   if (compareToSecretCombo.every((value) => (value === true))) {
-    lockState.locked = false;
+    lockState.locked = false
     redirect('kathy-morawski')
   }
 }
